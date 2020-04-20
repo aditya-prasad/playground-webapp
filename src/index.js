554d5aa40ad461b64e1991e69673af6150855af3
Aditya Prasad <aditya@okcredit.in>
2020-04-21 01:04:22 +0530

    change msg

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 ReactDOM.render(
     <div>
-        <h1>Hello, Karthik!!</h1>
+        <h1>Hello, World!</h1>
         <p>Environment: {window.config["env"]}</p>
     </div>,
     document.getElementById('root')
